58f64a4af0c223d5137fe11d80beb5206aab6dfb
Kyra Kocis <kkocis16@deerfield.edu>
2016-02-14 19:39:37 -0500

    make a new class that contains all of the Strings you need to use

---
+++ src/Globals.java
@@ -0,0 +1,17 @@
+import java.awt.Font;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class Globals
+{
+	protected static String hex= new String();
+	protected static String alpha= new String();
+	protected static String inputHex= new String();
+	
+	
+	public static String getInput= new String();
+	//public static String inputCharacter= new String();
+	
+		}
+	
+
